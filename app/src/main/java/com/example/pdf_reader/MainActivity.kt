065c2c593edd5d9c1bc58a682b8264702bd4204c
123Brijesh44aa <oddsworld0121@gmail.com>
2024-08-19 13:57:47 +0530

    initial commit

---
+++ app/src/main/java/com/example/pdf_reader/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.pdf_reader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pdf_reader.ui.theme.PdfReaderTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PdfReaderTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PdfReaderScreen(
+                        modifier =
+                        Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
